56988bcc508750289abde3638acd3270975898ae
Rafael Sant'Ana <rafael.santana@dcc.ufmg.br>
2025-09-17 13:36:53 -0300

    Problema 1: Empacotamento

--- empacotamento/tp1_2023087834.jl
+++ empacotamento/tp1_2023087834.jl
@@ -2,32 +2,31 @@ using JuMP
 using HiGHS
 
 mutable struct EmpacotamentoData
-  objetos::Int
-  pesos::Array{Int}
-  caixa_peso_maximo::Int
+    objetos::Int64           # n
+    pesos::Array{Float64}      # w_i
+    caixa_peso_maximo::Int64 # 20kg
 end
 
 function readData(file)
-  objetos = 0
-  pesos = []
-  caixa_peso_maximo = 20
-  print(file)
+    objetos = 0
+    pesos = []
+    caixa_peso_maximo = 20
 
-  for l in eachline(file)
-    q = split(l, " ")
-    if q[1] == "n"
-      n = parse(Int64, q[2])
-      objetos = n
+    for l in eachline(file)
+        q = split(l, "	")
+        if q[1] == "n"
+            n = parse(Int64, q[2])
+            objetos = n
+            pesos = [0.00 for i = 1:n]
+        elseif q[1] == "o" # descricao de objeto
+            id = parse(Int64, q[2])
+            peso = parse(Float64, q[3])
+
+            pesos[id+1] = peso
+        end
     end
-    if q[1] == "o" # descricao de objeto
-      id = parse(Int64, q[2])
-      preco = parse(Float64, q[3])
 
-      pesos[id] = preco
-    end
-  end
-
-  return EmpacotamentoData(objetos, pesos, caixa_peso_maximo)
+    return EmpacotamentoData(objetos, pesos, caixa_peso_maximo)
 end
 
 model = Model(HiGHS.Optimizer)
@@ -36,18 +35,15 @@ file = open(ARGS[1], "r")
 
 data = readData(file)
 
-print(data)
 # C = nossa variavel de quantas caixas vamos pegar
 
 @variable(model, c, Int)
 
-@constraint(model, sum(data.pesos[i] for i=1:data.objetos) <= c*data.caixa_peso_maximo)
+@constraint(model, sum(data.pesos[i] for i = 1:data.objetos) <= c * data.caixa_peso_maximo)
 
 @objective(model, Min, c)
-
-print(model)
 
 optimize!(model)
 
 sol = objective_value(model)
-println("Valor otimo= ", sol)
+println(sol)
